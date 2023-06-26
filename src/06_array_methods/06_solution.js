3f1d5215cd439a10ce0a0355419549443d8b91bf
dareaus <darauszhang@gmail.com>
2023-06-26 17:47:45 -0400

    s6 p2

--- src/06_array_methods/06_solution.js
+++ src/06_array_methods/06_solution.js
@@ -9,7 +9,7 @@ let useMapToUpperCase = function(string) {
 
 let useFilter = function(arr) {
     let output = arr.filter(function(string) {
-        string.includes("@") === true;
+        return string.includes("@");
     })
     return output;
 }
