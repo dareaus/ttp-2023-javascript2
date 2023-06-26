30f1f19e55b1fbc9fae14edc8fa16231f1e65b16
dareaus <darauszhang@gmail.com>
2023-06-26 18:24:00 -0400

    s7

--- src/07_thrice/07_solution.js
+++ src/07_thrice/07_solution.js
@@ -1,1 +1,16 @@
 // Code your solution here
+let thrice = function(f) {
+    let object = {
+        count: 3,
+        output: function (count) {
+            if (count > 0) {
+                f();
+            }
+            else {
+                return undefined;
+            }
+            count--;
+        }
+    }
+    return object.output;
+}
