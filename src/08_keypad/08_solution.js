30b7fcfb38bb5e3aa4938e6991f6df023851aca0
dareaus <darauszhang@gmail.com>
2023-06-26 19:14:44 -0400

    s8

--- src/08_keypad/08_solution.js
+++ src/08_keypad/08_solution.js
@@ -1,7 +1,7 @@
 // Code your solution here
 let presses = function(string) {
     let sum = 0;
-    let keypad = [[1], ["a", "b", "c", "2"], ["d", "e", "f", "3"], ["g", "h", "i", "4"], ["j", "k", "l", "5"], ["m", "n", "o", "6"], ["p", "q", "r", "s", "7"], ["t", "u", "v", "8"], ["w", "x", "y", "z", "9"], [" ", "0"]];
+    let keypad = [["1"], ["a", "b", "c", "2"], ["d", "e", "f", "3"], ["g", "h", "i", "4"], ["j", "k", "l", "5"], ["m", "n", "o", "6"], ["p", "q", "r", "s", "7"], ["t", "u", "v", "8"], ["w", "x", "y", "z", "9"], [" ", "0"]];
     for (let i = 0; i < string.length; i++) {
         for (let j = 0; j < keypad.length; j++) {
             if (keypad[j].includes(string[i].toLowerCase())) {
@@ -11,5 +11,4 @@ let presses = function(string) {
     }
     return sum;
 }
-
 
