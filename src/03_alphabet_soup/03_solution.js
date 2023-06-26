e15c33000f7a95e31e11ccb9e908b75ccdfc77c7
dareaus <darauszhang@gmail.com>
2023-06-26 15:44:01 -0400

    s3

--- src/03_alphabet_soup/03_solution.js
+++ src/03_alphabet_soup/03_solution.js
@@ -1,5 +1,8 @@
 // Code your solution here
 let soup = function(phrase, characterBank) {
+    if(characterBank.length <phrase.length) {
+        return(false);
+    }
     for (let i = 0; i < phrase.length; i++) {
         let check = false;
         for (let j = 0; j < characterBank.length; j++) {
