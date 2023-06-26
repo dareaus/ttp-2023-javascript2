8d9090e15488050d8952b8570e2562faa6c05542
dareaus <darauszhang@gmail.com>
2023-06-26 17:22:24 -0400

    s4

--- src/05_reject/05_solution.js
+++ src/05_reject/05_solution.js
@@ -1,1 +1,10 @@
 // Code your solution here
+let reject = function(arr, func) {
+    let rejected = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!(arr[i])) {
+            rejected.push(arr[i]);
+        }
+    }
+    return rejected;
+}
