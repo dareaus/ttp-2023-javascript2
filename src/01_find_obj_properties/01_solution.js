94f71b520284ecad5f8f6508be3b307d34990457
dareaus <darauszhang@gmail.com>
2023-06-26 14:09:54 -0400

    s1

--- src/01_find_obj_properties/01_solution.js
+++ src/01_find_obj_properties/01_solution.js
@@ -1,1 +1,10 @@
 // Code your solution here
+let findObjPropsHasOwn = function(object) {
+    let properties = [];
+    for (let key in object) {
+        if (object.hasOwnProperty(key)) {
+            properties.push(key);
+        }
+    }
+    return properties.join(", ");
+}
