8c6f2f9b40088465363c3117d376dfab118aa538
dareaus <darauszhang@gmail.com>
2023-06-26 15:37:20 -0400

    s2

--- src/03_alphabet_soup/03_solution.js
+++ src/03_alphabet_soup/03_solution.js
@@ -1,1 +1,16 @@
 // Code your solution here
+let soup = function(phrase, bank) {
+    for (let i = 0; i < phrase.length; i++) {
+        let check = False;
+        for (let j = 0; j < bank.length; j++) {
+            if (phrase[i] === bank[j]) {
+                check = True;
+                break;
+            }
+        }
+        if (!check) {
+            return(False);
+        }
+    }
+    return(True);
+}
