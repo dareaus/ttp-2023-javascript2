b58892358bb740ea6000735fed8608b92a33a164
dareaus <darauszhang@gmail.com>
2023-06-26 11:51:30 -0400

    s0

--- src/00_multiplication_table/00_solution.js
+++ src/00_multiplication_table/00_solution.js
@@ -1,1 +1,11 @@
 // Code your solution here
+let multiplicationTable = function(r, c) {
+    let table = [];
+    for (let p in r) {
+        row = [];
+        for (let q in c) {
+            row.push(p*q);
+        }
+        table.push(row);
+    }
+}
