174e509f9ec7209250b747265f1c5c0ef385a6fb
dareaus <darauszhang@gmail.com>
2023-06-26 17:31:49 -0400

    s6 p1

--- src/06_array_methods/06_solution.js
+++ src/06_array_methods/06_solution.js
@@ -1,1 +1,8 @@
 // Code your solution here
+let useMapToUppercase = function(string) {
+    let arr = string.split("");
+    let output = arr.map(function(char) {
+        return char.toUpperCase();
+    })
+    return output.join("");
+}
